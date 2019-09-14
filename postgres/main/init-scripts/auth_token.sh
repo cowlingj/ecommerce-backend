a94d1ee7d22f8fb1509d3689970f72345d746b92
cowlingj <09cowlingj@gmail.com>
2019-09-15 00:36:52 +0100

    Fix auth initialises

--- postgres/main/init-scripts/auth_token.sh
+++ postgres/main/init-scripts/auth_token.sh
@@ -1,9 +1,6 @@
-
-echo "user: $POSTGRES_USER"
-
 psql --username "$POSTGRES_USER" <<EOF
-CREATE DATABASE IF NOT EXISTS auth
-USE auth
+CREATE DATABASE auth;
+\c auth
 
 INSERT INTO authentication_keys (id, permissions) VALUES
   ('test-key', NULL);
