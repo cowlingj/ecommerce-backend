8e6832cbed7ebe453c636a231699723a97a87791
cowlingj <09cowlingj@gmail.com>
2019-09-24 13:42:25 +0100

    Fix cms routing

--- cms/main/src/routes/index.js
+++ cms/main/src/routes/index.js
@@ -1,7 +1,7 @@
 var keystone = require('keystone')
 
 exports = module.exports = function (app) {
-	app.get('/', (_req, res) => { res.redirect(keystone.get('admin path')) })
+	app.get(['/', '/cms'], (_req, res) => { res.redirect(keystone.get('admin path')) })
 	app.all('*', (_req, res) => { res.sendStatus(404) })
 	app.use((_err, _req, res, _next) => { res.sendStatus(500) })
 }
