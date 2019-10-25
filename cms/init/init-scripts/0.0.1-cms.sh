b5541daf91631f9da791c9e3c6523ef9855d7dc4
cowlingj <09cowlingj@gmail.com>
2019-10-25 20:56:34 +0100

    Test connection to mongodb

--- cms/init/init-scripts/0.0.1-cms.sh
+++ cms/init/init-scripts/0.0.1-cms.sh
@@ -1,9 +1,11 @@
-
-if [ mongo --username "$CMS_USERNAME" --password "$CMS_PASSWORD" --host "$DB_HOST" -- "$CMS_DB" ]; then
+if [ mongo --verbose --username "$CMS_USERNAME" --password "$CMS_PASSWORD" --host "$DB_HOST" -- "$CMS_DB" ]; then
+  echo "CMS database already exists"
   exit 0
 fi
 
-mongo --username "$ADMIN_USERNAME" --password "$ADMIN_PASSWORD" --host "$DB_HOST" -- "$ADMIN_DB" <<EOF
+set -x
+
+mongo --username "$ADMIN_USERNAME" --password "$ADMIN_PASSWORD" --host "$DB_HOST" --verbose -- "$ADMIN_DB" <<EOF
   db.createUser({
       user: "$CMS_USERNAME",
       pwd: "$CMS_PASSWORD",
