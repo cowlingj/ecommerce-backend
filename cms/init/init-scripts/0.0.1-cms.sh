48d2267c2a6842df2ccd020bdf7e27da9af20f7d
cowlingj <09cowlingj@gmail.com>
2019-10-25 20:17:54 +0100

    Fix connecting to mongodb helm config

--- cms/init/init-scripts/0.0.1-cms.sh
+++ cms/init/init-scripts/0.0.1-cms.sh
@@ -1,9 +1,9 @@
 
-if [ mongo --username "$ADMIN_USERNAME" --password "$ADMIN_PASSWORD" -- "$ADMIN_DB" ]; then
+if [ mongo --username "$CMS_USERNAME" --password "$CMS_PASSWORD" --host "$DB_HOST" -- "$CMS_DB" ]; then
   exit 0
 fi
 
-mongo --username "$ADMIN_USERNAME" --password "$ADMIN_PASSWORD" -- "$ADMIN_DB" <<EOF
+mongo --username "$ADMIN_USERNAME" --password "$ADMIN_PASSWORD" --host "$DB_HOST" -- "$ADMIN_DB" <<EOF
   db.createUser({
       user: "$CMS_USERNAME",
       pwd: "$CMS_PASSWORD",
