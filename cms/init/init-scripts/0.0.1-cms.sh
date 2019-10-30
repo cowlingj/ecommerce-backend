0d220e440ca8259e2140857f3f495961c06a3fa5
cowlingj <09cowlingj@gmail.com>
2019-10-30 21:40:45 +0000

    Fix mongodb cms config

--- cms/init/init-scripts/0.0.1-cms.sh
+++ cms/init/init-scripts/0.0.1-cms.sh
@@ -1,13 +1,11 @@
-if mongo --verbose --username "$CMS_USERNAME" --password "$CMS_PASSWORD" --host "$DB_HOST" -- "$CMS_DB"; then
+if mongo --verbose --username "$CMS_USERNAME" --password "$CMS_PASSWORD" --host "$DB_HOST" --authenticationDatabase "$ADMIN_DB" -- "$CMS_DB"; then
   echo "database \"$CMS_DB\" already exists, exiting"
   exit 0
 fi
 
 echo "database \"$CMS_DB\" doesn't exist, creating (entering verbose mode)"
 
-set -xv
-
-mongo --username "$ADMIN_USERNAME" --password "$ADMIN_PASSWORD" --host "$DB_HOST" --verbose <<EOF
+mongo --username "$ADMIN_USERNAME" --password "$ADMIN_PASSWORD" --host "$DB_HOST" --verbose -- "$ADMIN_DB" <<EOF
   db.createUser({
       user: "$CMS_USERNAME",
       pwd: "$CMS_PASSWORD",
