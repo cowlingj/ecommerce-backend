0cb01370ddc33bfdde8f91a40f591f25fca7b1d3
cowlingj <09cowlingj@gmail.com>
2019-10-30 16:10:13 +0000

    Fix remove debug logs from init script

--- cms/init/init-scripts/0.0.1-cms.sh
+++ cms/init/init-scripts/0.0.1-cms.sh
@@ -5,20 +5,12 @@ fi
 
 echo "database \"$CMS_DB\" doesn't exist, creating (entering verbose mode)"
 
-echo "ADMIN CREDS: $ADMIN_USERNAME $ADMIN_PASSWORD"
-echo "CMS CREDS: $CMS_USERNAME $CMS_PASSWORD"
-
-set -xv
-
 mongo --username "$ADMIN_USERNAME" --password "$ADMIN_PASSWORD" --host "$DB_HOST" --verbose <<EOF
-  db.getName();
-  print("$CMS_USERNAME");
-  print("$CMS_PASSWORD");
   db.createUser({
       user: "$CMS_USERNAME",
       pwd: "$CMS_PASSWORD",
       roles: [
         { role: "dbOwner", db: "$CMS_DB" }
       ]
-    })
+  });
 EOF
