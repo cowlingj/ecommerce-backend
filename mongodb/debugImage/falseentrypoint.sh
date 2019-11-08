1d85a0441de5bd2d1b8049986c2b45d41ae2c3aa
cowlingj <09cowlingj@gmail.com>
2019-11-08 19:24:18 +0000

    Fix add wrapper to mongodb debug entrypoint

---
+++ mongodb/debugImage/falseentrypoint.sh
@@ -0,0 +1,13 @@
+
+if [[ -n "$MONGODB_ROOT_PASSWORD" ]] || [[ -n "$MONGODB_PASSWORD" ]]; then
+  authorization="$(yq read "$MONGODB_CONF_FILE" security.authorization)"
+  if [[ "$authorization" = "disabled" ]]; then
+    echo "should turn auth on"
+  else
+    echo "auth is already on"
+  fi
+else
+  echo "root pw ($MONGODB_ROOT_PASSWORD) and pw ($MONGODB_PASSWORD) not set, no auth"
+fi
+
+. /entrypoint.sh
