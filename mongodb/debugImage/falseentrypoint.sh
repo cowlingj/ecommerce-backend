6e5b987bcf5cf65bf7410e9204f7a59ca8e931e3
cowlingj <09cowlingj@gmail.com>
2019-11-11 00:00:40 +0000

    Fix read mongodb.conf file

--- mongodb/debugImage/falseentrypoint.sh
+++ mongodb/debugImage/falseentrypoint.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+echo "conf before"
+cat "/opt/bitnami/mongodb/conf/mongodb.conf" >&2
+
 if [[ -n "$MONGODB_ROOT_PASSWORD" ]] || [[ -n "$MONGODB_PASSWORD" ]]; then
-  authorization="$(yq read "$MONGODB_CONF_FILE" security.authorization)"
+  authorization="$(yq read "/opt/bitnami/mongodb/conf/mongodb.conf" security.authorization)"
   if [[ "$authorization" = "disabled" ]]; then
     echo "should turn auth on"
   else
@@ -10,5 +13,8 @@ if [[ -n "$MONGODB_ROOT_PASSWORD" ]] || [[ -n "$MONGODB_PASSWORD" ]]; then
 else
   echo "root pw ($MONGODB_ROOT_PASSWORD) and pw ($MONGODB_PASSWORD) not set, no auth"
 fi
+
+echo "conf after"
+cat "/opt/bitnami/mongodb/conf/mongodb.conf" >&2
 
 . /entrypoint.sh /run.sh
