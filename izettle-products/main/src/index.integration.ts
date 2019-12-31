31145d5d09bc713c92975f3ad06e823a7e14e443
cowlingj <09cowlingj@gmail.com>
2019-12-31 12:12:33 +0000

    [izp] Add oauth config

--- izettle-products/main/src/index.integration.ts
+++ izettle-products/main/src/index.integration.ts
@@ -1,34 +1,73 @@
 import { ApolloServer } from "apollo-server";
-import ApolloClient, { gql } from 'apollo-boost';
-import fetch from 'node-fetch'
+import ApolloClient, { gql } from "apollo-boost";
+import fetch from "node-fetch";
+import http from "http";
+import { promisify } from "util";
+import connect from "connect";
+import path from "path";
+import fs from "fs";
+import { json } from "body-parser";
 
 describe("Server", () => {
-  let server: ApolloServer
-  let client: ApolloClient<undefined>
+  let serversToCloseAfterEachTest: http.Server[] = [];
+  let server: ApolloServer;
+  let client: ApolloClient<undefined>;
   let originalHost: string | undefined;
   let originalPort: string | undefined;
+  let testCredentials: {
+    username: string;
+    password: string;
+    client_id: string;
+    client_secret: string;
+  };
 
-  beforeAll(() => {
+  beforeAll(async () => {
     originalHost = process.env.HOST;
     originalPort = process.env.PORT;
 
     process.env.PORT = "3000";
     process.env.HOST = "localhost";
+    process.env.IZETTLE_PRODUCTS_URI = "http://localhost:3001";
+    process.env.IZETTLE_AUTH_URI = "http://localhost:3002";
+    process.env.SECRETS_DIR = path.resolve(process.cwd(), "test", "secrets");
+    process.env.IZETTLE_CREDENTIALS_FILE = "izettle_credentials.json";
+
+    testCredentials = JSON.parse(
+      fs.readFileSync(
+        path.resolve(
+          process.env.SECRETS_DIR,
+          process.env.IZETTLE_CREDENTIALS_FILE
+        ),
+        "utf-8"
+      )
+    );
 
     server = require("./index").default;
 
     client = new ApolloClient({
       uri: `http://${process.env.HOST}:${process.env.PORT}`,
-      fetch
-    })
+      fetch: (fetch as unknown) as (
+        input: RequestInfo,
+        init?: RequestInit | undefined
+      ) => Promise<Response>
+    });
 
     process.env.HOST = originalHost;
     process.env.PORT = originalPort;
   });
 
   afterAll(async () => {
-    await server.stop();
     client.stop();
+    await server.stop();
+  });
+
+  afterEach(async () => {
+    await Promise.all(
+      serversToCloseAfterEachTest.map(server =>
+        promisify((cb: (err?: Error) => void) => server.close(cb))()
+      )
+    );
+    serversToCloseAfterEachTest = [];
   });
 
   it("is running at uri", async () => {
@@ -45,25 +84,127 @@ describe("Server", () => {
     });
 
     expect(res.data).toMatchObject({
-      "__schema": {
-        "queryType": {
-          "name": "Query"
+      __schema: {
+        queryType: {
+          name: "Query"
         }
       }
-    })
-  })
+    });
+  });
 
   it("Can get a list of products", async () => {
+    const token = "token";
+    const products = [
+      {
+        uuid: "uuid-1",
+        name: "name-1",
+        description: "",
+        variants: [
+          {
+            uuid: "uuid",
+            name: "",
+            description: "",
+            sku: "SKU123",
+            price: {
+              amount: 1,
+              currencyId: "GBP"
+            }
+          }
+        ],
+        unitName: null,
+        online: {
+          status: "ACTIVE|HIDDEN",
+          title: null,
+          description: null,
+          shipping: {
+            shippingPricingModel: "FREE|STANDARD"
+          }
+        }
+      }
+    ];
+
+    let getTokenCalls = 0;
+    serversToCloseAfterEachTest.push(
+      connect()
+        .use(json())
+        .use(
+          "/token/",
+          (
+            req: { body: unknown } | http.IncomingMessage,
+            res: http.ServerResponse,
+            next: Function
+          ) => {
+            if (req.method !== "POST") {
+              return next();
+            }
+
+            getTokenCalls++;
+
+            if (
+              req.body["username"] !== testCredentials.username ||
+              req.body["password"] !== testCredentials.password ||
+              req.body["grant_type"] !== "password" ||
+              req.body["client_id"] !== testCredentials.client_id ||
+              req.body["client_secret"] !== testCredentials.client_secret
+            ) {
+              throw new Error(
+                `client oauth get token body incorrect ${req.body}`
+              );
+            }
+            res.setHeader("Content-Type", "application/json");
+            res.end(
+              JSON.stringify({
+                'access_token': token,
+                'refresh_token': null,
+                'expires_in': null
+              })
+            );
+          }
+        )
+        .listen(3002)
+    );
+
+    let allProductsCalls = 0;
+    serversToCloseAfterEachTest.push(
+      connect()
+        .use(
+          "/organizations/self/products/v2/",
+          (
+            req: http.IncomingMessage,
+            res: http.ServerResponse,
+            next: Function
+          ) => {
+            if (req.method !== "GET") {
+              return next();
+            }
+
+            allProductsCalls++;
+            if (req.headers.authorization !== `Bearer ${token}`) {
+              throw new Error(
+                "no valid bearer token used for get all products"
+              );
+            }
+            res.end(JSON.stringify({ data: products }));
+          }
+        )
+        .listen(3001)
+    );
+
     const res = await client.query({
       query: gql`
         {
           allProducts {
             id
+            name
           }
         }
       `
     });
 
-    expect(res.data).toMatchObject({ allProducts: []})
+    expect(allProductsCalls).toBe(1);
+    expect(getTokenCalls).toBe(1);
+    expect(res.data).toMatchObject({
+      allProducts: [{ id: "uuid-1", name: "name-1" }]
+    });
   });
 });
--- izettle-products/main/src/index.ts
+++ izettle-products/main/src/index.ts
@@ -1,19 +1,88 @@
 import { ApolloServer } from 'apollo-server'
 import dotenv from 'dotenv'
 import typeDefs from '@/schema.gql'
+import fetch from 'node-fetch'
+import fs from 'fs'
+import path from 'path'
+import oauth2 from 'simple-oauth2'
+
+if (
+  process.env.IZETTLE_AUTH_URI === undefined
+  || process.env.IZETTLE_PRODUCTS_URI === undefined
+  || process.env.IZETTLE_CREDENTIALS_FILE === undefined
+  || process.env.SECRETS_DIR === undefined
+) {
+  throw new Error('environment variables not set up')
+}
 
 dotenv.config()
 
+const credentials: {
+  'username': string;
+  'password': string;
+  'client_id': string;
+  'client_secret': string;
+} = JSON.parse(fs.readFileSync(
+  path.resolve(
+    process.env.SECRETS_DIR,
+    process.env.IZETTLE_CREDENTIALS_FILE
+  ),
+  'utf8'
+))
+
+const oauth2Client = oauth2.create({
+  client: {
+    id: credentials.client_id,
+    secret: credentials.client_secret
+  },
+  auth: {
+    tokenHost: process.env.IZETTLE_AUTH_URI,
+    tokenPath: '/token'
+  },
+  options: {
+    bodyFormat: 'json',
+    authorizationMethod: 'body'
+  }
+})
+
 const resolvers = {
   Query: {
-    allProducts: (): never[] => [],
+    allProducts: async (): Promise<never[]> => {
+
+      const token = oauth2Client.accessToken.create(
+        await oauth2Client.ownerPassword.getToken({
+          username: credentials.username,
+          password: credentials.password,
+          scope: ''
+        })
+      ).token['access_token']
+
+
+      const res = await fetch(
+        `${process.env.IZETTLE_PRODUCTS_URI}/organizations/self/products/v2/`,
+        {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        }
+      )
+      if (!res.ok) {
+        throw new Error(`get all products failed (status: ${res.status})`)
+      }
+      return (await res.json()).data.map((izettleProduct: { uuid: string; name: string}) => {
+        return {
+          id: izettleProduct.uuid,
+          name: izettleProduct.name
+        };
+      })
+    },
   },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers, playground: process.env.NODE_ENV === 'development' });
 
 server.listen({
-  port: parseInt(process.env.PORT as string) || 80,
+  port: parseInt(process.env.PORT ?? '') || 80,
   host: process.env.HOST ?? "0.0.0.0"
 }).then(({ url }: { url: string }) => {
   console.log(`Server ready at ${url}`);
