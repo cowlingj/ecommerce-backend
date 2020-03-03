cc77ebbb2daf860defbfaad952e5454e07670342
cowlingj <09cowlingj@gmail.com>
2020-03-03 20:08:52 +0000

    [spe] Fix include env in html template directly

--- examples/simple/app/www/env.js
+++
@@ -1,6 +0,0 @@
-// Apache will rewrite these
-const env = {
-  PRODUCTS_URI: '<!--#echo var="PRODUCTS_URI" -->',
-  RESOURCES_URI: '<!--#echo var="RESOURCES_URI" -->',
-  EVENTS_URI: '<!--#echo var="EVENTS_URI" -->'
-}
