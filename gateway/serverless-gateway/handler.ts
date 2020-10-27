f7d0863dbf160b78a9fda42c5b59af8077033183
cowlingj <09cowlingj@gmail.com>
2020-10-27 23:59:30 +0000

    Fix gateway handler

--- gateway/serverless-gateway/handler.ts
+++ gateway/serverless-gateway/handler.ts
@@ -2,4 +2,6 @@ import 'source-map-support/register';
 import serverless from 'serverless-http';
 import createApp from './app';
 
-export default createApp().then(serverless)
+const asyncHandler = createApp().then(serverless)
+
+export default async (event, ctx) => ((await asyncHandler)(event, ctx))
